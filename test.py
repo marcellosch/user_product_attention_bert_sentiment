268f2c6b0f0a7f8a52d4e339ef308e5dc4442b41
uniquefine <finally@lepus.uberspace.de>
2019-05-24 19:58:34 +0800

    fixed tests, they pass now

--- test.py
+++ test.py
@@ -14,11 +14,12 @@ class TestModel(unittest.TestCase):
 
     def test_SimpleUserProductBert(self):
         supb = SimpleUserProductBert(n_user=100, n_product=200, n_classes=5)
-        input_ids = (torch.rand(64,512)*800).long()
-        input_mask = torch.ones(64,512).long()
-        user_ids = (torch.rand(64) * 100).long()
-        product_ids = (torch.rand(64)*200).long()
+        input_ids = (torch.rand(2,512)*800).long()
+        input_mask = torch.ones(2,512).long()
+        user_ids = (torch.rand(2) * 100).long()
+        product_ids = (torch.rand(2)*200).long()
         out = supb(input_ids, input_mask, user_ids, product_ids)
+        self.assertEqual(out.shape, (2,5))
         
 
 if __name__ == '__main__':
