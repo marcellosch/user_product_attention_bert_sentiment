0df8dd64374d770ef50c32a6a2be900a0ef8165a
uniquefine <finally@lepus.uberspace.de>
2019-05-29 14:59:56 +0800

    Added layer that loads bert word embeddings from pretrained

---
+++ bert_word_embedding.py
@@ -0,0 +1,158 @@
+from pytorch_pretrained_bert.modeling import BertEmbeddings
+from pytorch_pretrained_bert.modeling import BertConfig
+from pytorch_pretrained_bert.modeling import PRETRAINED_MODEL_ARCHIVE_MAP, BERT_CONFIG_NAME
+from pytorch_pretrained_bert.file_utils import cached_path, WEIGHTS_NAME, CONFIG_NAME
+import tempfile
+import logging
+import os
+import tarfile
+import torch
+import shutil
+
+log_format = '%(asctime)-10s: %(message)s'
+logging.basicConfig(level=logging.INFO, format=log_format)
+ 
+logger = logging.getLogger(__name__)
+
+
+
+class BertWordEmbeddings(BertEmbeddings):
+    def forward(self, input_ids, token_type_ids=None):
+        return self.word_embeddings(input_ids)
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_name_or_path, *inputs, **kwargs):
+        """
+        Instantiate a BertWordEmbeddings from a pre-trained model file or a pytorch state dict.
+        Download and cache the pre-trained model file if needed.
+        Params:
+            pretrained_model_name_or_path: either:
+                - a str with the name of a pre-trained model to load selected in the list of:
+                    . `bert-base-uncased`
+                    . `bert-large-uncased`
+                    . `bert-base-cased`
+                    . `bert-large-cased`
+                    . `bert-base-multilingual-uncased`
+                    . `bert-base-multilingual-cased`
+                    . `bert-base-chinese`
+                - a path or url to a pretrained model archive containing:
+                    . `bert_config.json` a configuration file for the model
+                    . `pytorch_model.bin` a PyTorch dump of a BertForPreTraining instance
+                - a path or url to a pretrained model archive containing:
+                    . `bert_config.json` a configuration file for the model
+                    . `model.chkpt` a TensorFlow checkpoint
+            from_tf: should we load the weights from a locally saved TensorFlow checkpoint
+            cache_dir: an optional path to a folder in which the pre-trained models will be cached.
+            state_dict: an optional state dictionnary (collections.OrderedDict object) to use instead of Google pre-trained models
+            *inputs, **kwargs: additional input for the specific Bert class
+                (ex: num_labels for BertForSequenceClassification)
+        """
+        state_dict = kwargs.get('state_dict', None)
+        kwargs.pop('state_dict', None)
+        cache_dir = kwargs.get('cache_dir', None)
+        kwargs.pop('cache_dir', None)
+        from_tf = kwargs.get('from_tf', False)
+        kwargs.pop('from_tf', None)
+
+        if pretrained_model_name_or_path in PRETRAINED_MODEL_ARCHIVE_MAP:
+            archive_file = PRETRAINED_MODEL_ARCHIVE_MAP[pretrained_model_name_or_path]
+        else:
+            archive_file = pretrained_model_name_or_path
+        # redirect to the cache, if necessary
+        try:
+            resolved_archive_file = cached_path(
+                archive_file, cache_dir=cache_dir)
+        except EnvironmentError:
+            logger.error(
+                "Model name '{}' was not found in model name list ({}). "
+                "We assumed '{}' was a path or url but couldn't find any file "
+                "associated to this path or url.".format(
+                    pretrained_model_name_or_path,
+                    ', '.join(PRETRAINED_MODEL_ARCHIVE_MAP.keys()),
+                    archive_file))
+            return None
+        if resolved_archive_file == archive_file:
+            logger.info("loading archive file {}".format(archive_file))
+        else:
+            logger.info("loading archive file {} from cache at {}".format(
+                archive_file, resolved_archive_file))
+        tempdir = None
+        if os.path.isdir(resolved_archive_file) or from_tf:
+            serialization_dir = resolved_archive_file
+        else:
+            # Extract archive to temp dir
+            tempdir = tempfile.mkdtemp()
+            logger.info("extracting archive file {} to temp dir {}".format(
+                resolved_archive_file, tempdir))
+            with tarfile.open(resolved_archive_file, 'r:gz') as archive:
+                archive.extractall(tempdir)
+            serialization_dir = tempdir
+        # Load config
+        config_file = os.path.join(serialization_dir, CONFIG_NAME)
+        if not os.path.exists(config_file):
+            # Backward compatibility with old naming format
+            config_file = os.path.join(serialization_dir, BERT_CONFIG_NAME)
+        config = BertConfig.from_json_file(config_file)
+        logger.info("Model config {}".format(config))
+        # Instantiate model.
+        #model = cls(config, *inputs, **kwargs)
+        emb = cls(config, *inputs, **kwargs)
+        if state_dict is None and not from_tf:
+            weights_path = os.path.join(serialization_dir, WEIGHTS_NAME)
+            state_dict = torch.load(weights_path, map_location='cpu')
+        if tempdir:
+            # Clean up temp dir
+            shutil.rmtree(tempdir)
+        if from_tf:
+            # Directly load from a TensorFlow checkpoint
+            weights_path = os.path.join(serialization_dir, TF_WEIGHTS_NAME)
+            raise RuntimeError("Loading from tensorflow weights is not supported")
+            #return load_tf_weights_in_bert(model, weights_path)
+        # Load from a PyTorch state_dict
+        old_keys = []
+        new_keys = []
+        for key in state_dict.keys():
+            new_key = None
+            if 'gamma' in key:
+                new_key = key.replace('gamma', 'weight')
+            if 'beta' in key:
+                new_key = key.replace('beta', 'bias')
+            if new_key:
+                old_keys.append(key)
+                new_keys.append(new_key)
+        for old_key, new_key in zip(old_keys, new_keys):
+            state_dict[new_key] = state_dict.pop(old_key)
+
+        missing_keys = []
+        unexpected_keys = []
+        error_msgs = []
+        # copy state_dict so _load_from_state_dict can modify it
+        metadata = getattr(state_dict, '_metadata', None)
+        state_dict = state_dict.copy()
+        if metadata is not None:
+            state_dict._metadata = metadata
+
+        def load(module, prefix=''):
+            local_metadata = {} if metadata is None else metadata.get(
+                prefix[:-1], {})
+            module._load_from_state_dict(
+                state_dict, prefix, local_metadata, True, missing_keys, unexpected_keys, error_msgs)
+            for name, child in module._modules.items():
+                if child is not None:
+                    load(child, prefix + name + '.')
+        start_prefix = 'bert.embeddings'
+        # if not hasattr(model, 'bert') and any(s.startswith('bert.') for s in state_dict.keys()):
+        #    start_prefix = 'bert.'
+        load(emb, prefix=start_prefix)
+        # if len(missing_keys) > 0:
+        #    logger.info("Weights of {} not initialized from pretrained model: {}".format(
+        #        model.__class__.__name__, missing_keys))
+        # if len(unexpected_keys) > 0:
+        #    logger.info("Weights from pretrained model not used in {}: {}".format(
+        #        model.__class__.__name__, unexpected_keys))
+        # if len(error_msgs) > 0:
+        #    raise RuntimeError('Error(s) in loading state_dict for {}:\n\t{}'.format(
+        #                       model.__class__.__name__, "\n\t".join(error_msgs)))
+        # return model
+        return emb
+
--- test.py
+++ test.py
@@ -6,37 +6,43 @@ from modelbaselinebert import *
 from modelbaselineupa import *
 from model_simple_user_product_bert import SimpleUserProductBert
 
+
 class TestModel(unittest.TestCase):
     def test_UserProductAttention_dimensions(self):
         upa = UserProductAttention()
-        H = torch.rand(64,512,768)
-        u = torch.rand(64,200)
-        p = torch.rand(64,200)
-        out = upa(H,u,p)
-        self.assertEqual(out.shape,(64,768))
+        H = torch.rand(64, 512, 768)
+        u = torch.rand(64, 200)
+        p = torch.rand(64, 200)
+        out = upa(H, u, p)
+        self.assertEqual(out.shape, (64, 768))
 
     def test_SimpleUserProductBert(self):
         supb = SimpleUserProductBert(n_user=100, n_product=200, n_classes=5)
-        input_ids = (torch.rand(2,512)*800).long()
-        input_mask = torch.ones(2,512).long()
+        input_ids = (torch.rand(2, 512)*800).long()
+        input_mask = torch.ones(2, 512).long()
         user_ids = (torch.rand(2) * 100).long()
         product_ids = (torch.rand(2)*200).long()
         out = supb(input_ids, input_mask, user_ids, product_ids)
-        self.assertEqual(out.shape, (2,5))
+        self.assertEqual(out.shape, (2, 5))
 
     def test_VanillaBert(self):
         vbert = VanillaBert(n_classes=5)
-        input_ids = (torch.rand(2,512)*800).long()
-        input_mask = torch.ones(2,512).long()
+        input_ids = (torch.rand(2, 512)*800).long()
+        input_mask = torch.ones(2, 512).long()
         out = vbert(input_ids, input_mask)
-        self.assertEqual(out.shape,(2,5))
+        self.assertEqual(out.shape, (2, 5))
 
     def test_VanillaUPA(self):
-        vupa = VanillaUPA(n_user=100, n_product=200, n_words=30522, n_classes=5)
-        input_ids = (torch.rand(2,512)*800).long()
-        input_mask = torch.ones(2,512).long()
+        vupa = VanillaUPA(n_user=100, n_product=200,
+                          n_words=30522, n_classes=5)
+        input_ids = (torch.rand(2, 512)*800).long()
+        input_mask = torch.ones(2, 512).long()
         out = vupa(input_ids, input_mask)
-        self.assertEqual(out.shape,(2,5))
+        self.assertEqual(out.shape, (2, 5))
+
+    def test_BertWordEmbeddings(self):
+        from bert_word_embedding import BertWordEmbeddings
+        b = BertWordEmbeddings.from_pretrained('bert-base-uncased')
 
 
 if __name__ == '__main__':
