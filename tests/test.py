9e042709bdc2cd611aac096d14f9e642ca7c75ee
John Thompson <jthompson4004@gmail.com>
2019-05-30 14:16:58 +0800

    testing repair...
    Former-commit-id: ca22f6dda2e7d40eb7bc39df3b2884e1b8c177f9 [formerly 4e428fb63a1797db8828a30c15c79becbbc42a3c] [formerly 1a81448f84b2ccafa057be7cb3733f816e885db8 [formerly 1f29c8b807e1d2b025fd99ba5d8ec045e33ef661]]
    Former-commit-id: 8ff7297cd3b1a5ccf13f5e0c4de47180f745d423 [formerly 26b165d37a47854b070e0f878038594be1cce08a]
    Former-commit-id: 0d648eac48b9b555d4b487ec9a5c08ae81e033bd
    Former-commit-id: bca6a194a7d396fc1916ded68c167621c102470d

--- tests/test.py
+++ tests/test.py
@@ -23,7 +23,7 @@ class TestModel(unittest.TestCase):
         user_ids = (torch.rand(2) * 100).long()
         product_ids = (torch.rand(2)*200).long()
         batch = (user_ids, product_ids, None,
-                 input_ids, None, input_mask, None)
+                 input_ids, None, input_mask, None, None)
         out = supb(batch)
         self.assertEqual(out.shape, (2, 5))
 
@@ -31,18 +31,17 @@ class TestModel(unittest.TestCase):
         vbert = VanillaBert(n_classes=5)
         input_ids = (torch.rand(2, 512)*800).long()
         input_mask = torch.ones(2, 512).long()
-        batch = (None, None, None, input_ids, None, input_mask, None)
+        batch = (None, None, None, input_ids, None, input_mask, None, None)
         out = vbert(batch)
         self.assertEqual(out.shape, (2, 5))
 
     def test_VanillaUPA(self):
-        vupa = VanillaUPA(n_user=100, n_product=200, n_classes=5,
-                          max_sentence_count=10, hidden_size=768)
+        vupa = VanillaUPA(n_user=100, n_product=200, n_classes=5, hidden_size=768)
         user_ids = (torch.rand(5)).long()
         product_ids = (torch.rand(5)).long()
         sentence_matrix = (torch.rand(10 * 5, 5)).long()
         batch = (user_ids, product_ids, None,
-                 None, None, None, sentence_matrix)
+                 None, None, None, sentence_matrix, 5)
         out = vupa(batch)
         print(out.shape)
         #self.assertEqual(out.shape, (5, 5))
