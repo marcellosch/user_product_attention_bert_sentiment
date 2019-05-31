ca31fceeab6dc61f39e6e62802e51da44f7e2fe0
Ubuntu <sebastian@main.5djo25ypp14uvn1oecy02jq32f.bx.internal.cloudapp.net>
2019-05-31 10:36:40 +0000

    newline
    Former-commit-id: 45d84f47798337020afa6e507b217ffe7d567731 [formerly 361a57876f262cc6ced4c430585f8fc6ca6825a2] [formerly ba8d7a8490483e5d9d321e143583f69b76b7e276 [formerly e2e3ff5e3aa6bdb28714878cfae3ebc991d31a60]]
    Former-commit-id: 3fcef349ac982b8ddb228b81cd20182d90331d30 [formerly 65e5bf4bf5d9d47cb90356b3583450e9f648c0fd]
    Former-commit-id: 30f6500e69ba58e6c6fd5f4e7bd31f570995da37
    Former-commit-id: 8f02c0948fb172509338712fed2025c9b3a1c6d9

--- model/train/train.py
+++ model/train/train.py
@@ -260,7 +260,7 @@ def train(model, train_dat, dev_dat, args, use_cat_collate=False):
                 if (step+1) % 1000 == 0:
                     dev_acc, dev_loss = eval_on_data(model, dev_dat, args.eval_batch_size, device, use_cat_collate=use_cat_collate, step=step)
                     train_acc, train_loss = eval_on_data(model, train_dat , args.eval_batch_size, device, use_cat_collate=use_cat_collate, step=step)
-                    logging.info("Step: {} Training loss: {}, acc: {}, Dev loss: {}, acc: {}\n".format(step, train_loss, train_acc, dev_loss, dev_acc))
+                    logging.info("Step: {} Training loss: {}, acc: {}, Dev loss: {}, acc: {}\n\n".format(step, train_loss, train_acc, dev_loss, dev_acc))
                     model.train()
 
         logging.info("***** Running evaluation on train set *****")
