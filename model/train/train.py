fixed accuracy calculation
Former-commit-id: 34cf31e8484b3df2a4525312aad9c5db11e3b466 [formerly 46d85f61fe123e2f71bd31ef56332ed00b8ef04d] [formerly 6f9bccb56fa6d18de30092b929f3e1a3a46bdb4e [formerly 4a880d8a858964bead9c7d7aa91bc20ff7ead69e]]
Former-commit-id: 4833f114e8c25b776bb367c8d2a3823aa7396811 [formerly 4da90e6c186854f36faf3d4c641158ef0b0c30a5]
Former-commit-id: 6d621c6d0991dc76e83c0d02b71dbc4018dbcd30
Former-commit-id: 4b9c96c65f3dff96a6e5e5ecda1900ce0c072e83

model/train/train.py
@@ -65,15 +65,17 @@ def eval_on_data(model, data, args, device, use_cat_collate=False):
 
         eval_loss += tmp_eval_loss.mean().item()
         nb_eval_steps += 1
-        if len(preds) == 0:
-            preds.append(logits.detach().cpu().numpy())
-        else:
-            preds[0] = np.append(
-                preds[0], logits.detach().cpu().numpy(), axis=0)
+        
+        # if len(preds) == 0:
+        #     preds.append(logits.detach().cpu().numpy())
+        # else:
+        #     preds[0] = np.append(
+        #         preds[0], logits.detach().cpu().numpy(), axis=0)
+        preds += logits.cpu().argmax(dim=1).tolist()
         labels += label.tolist()
 
-    preds = preds[0]
-    preds = np.argmax(preds, axis=1)
+    #preds = preds[0]
+    preds = np.array(preds)
     labels = np.array(labels)
 
     assert len(preds) == len(labels)

utils/data.py
@@ -94,9 +94,14 @@ class SentimentDataset(Dataset):
         sentences = []
         for end, token in enumerate(tokenized):
             if token == '[SEP]':
-                sentences += tokenized[begin:end]
+                sentences.append(tokenized[begin:end])
                 max_sentence_length = max(max_sentence_length, end-begin)
                 begin = end+1
+        if len(tokenized)-begin > 0:
+            sentences.append(tokenized[begin:])
+        
+        if len(sentences) == 0:
+            pdb.set_trace()
 
         sentences = [self.tokenizer.convert_tokens_to_ids(sentence) for sentence in sentences]
         return sentences, max_sentence_length
@@ -157,7 +162,7 @@ class SentimentDataset(Dataset):
 
         self.count_long_text = 0
         max_sentence_count = 0
-        for i, line in enumerate(lines[:100]):
+        for i, line in enumerate(lines):
             user_id, product_id, label, text = line
 
             label = int(label)-1  # classes are from 0-4 but starts from 1-5
@@ -253,6 +258,8 @@ class SentenceMatrixDataset(SentimentDataset):
         ret = []
         for sentence in self.documents["input_tokens"][idx]:
             ret.append(torch.tensor(sentence))
+        if len(ret) == 0:
+            pdb.set_trace()
 
         return pad_sequence(ret, batch_first=True)
 