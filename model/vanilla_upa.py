fix dimension of softmax input
Former-commit-id: af770555b924c0b8beb9c2081d4588b1faff6290 [formerly 476e4f2a717b2ea5d0db38f696e413c18b536654] [formerly 3cd6335227da5c51bd1e268e5169ce4b9f2ec6cf [formerly 80ff1320a0199ddd9b361cd8901ffe08c3b0658a]]
Former-commit-id: d6e962f76b8476078a6be346a7df1e32ca533634 [formerly 3fa3fc1740794cd5f23003bb920dcb5beda99e65]
Former-commit-id: 54df96ff2a7aa3251acccc7e8c1530aaadc32388
Former-commit-id: c7b4032b3be31165979072cf29c20b84f8b52b24

model/vanilla_upa.py
@@ -52,7 +52,7 @@ class VanillaUPA(torch.nn.Module):
         sentence_attention_out = self.sentence_attention(
             lstm2_out, user_embs, product_embs)
         linear_out = self.linear(sentence_attention_out)
-        softmax_out = self.softmax(linear_out)
+        softmax_out = self.softmax(linear_out.view(-1, self.n_classes))
         return softmax_out
 
     def train(self):