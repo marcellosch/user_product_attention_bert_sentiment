fixed softmax input and overwrote to method in upabert
Former-commit-id: 6dc9603695ebc75422f3d816bee81ee73969117a [formerly 511146885a647a5c3980abd2c588efcb946a6ac8] [formerly c5943219348a1c83f7c43bba606459b521569d1b [formerly 6c66971c6962fd5331f95cce5777a5b17a7e406c]]
Former-commit-id: d053c4d1c0772e76e5dc8083fd5726abde57f497 [formerly 91fd986c43ccce595d85142fd2b2fab1305b4bcf]
Former-commit-id: 5de66a131d897966c7530c717a413ca420fe23ef
Former-commit-id: 40d0a08f03b8b40f48a9a05f41c8b08922a78149

model/upa_bert.py
@@ -26,7 +26,7 @@ class UPABert(torch.nn.Module):
         self.lstm = torch.nn.LSTM(hidden_size, hidden_size)
         self.sentence_attention = UserProductAttention()
         self.linear = torch.nn.Linear(self.hidden_size, self.n_classes)
-        self.softmax = torch.nn.LogSoftmax()
+        self.softmax = torch.nn.LogSoftmax(dim=1)
 
     def forward(self, batch):
         """ Inputs:
@@ -50,7 +50,7 @@ class UPABert(torch.nn.Module):
         sentence_attention_out = self.sentence_attention(
             lstm_out, user_embs, product_embs)
         linear_out = self.linear(sentence_attention_out)
-        softmax_out = self.softmax(linear_out)
+        softmax_out = self.softmax(linear_out.view(-1, self.n_classes))
         return softmax_out
 
     def train(self):
@@ -58,3 +58,11 @@ class UPABert(torch.nn.Module):
 
     def eval(self):
         self.bert.eval()
+
+    def to(self, *args, **kwargs):
+        self = super().to(*args, **kwargs)
+        self.word_attention.to(*args, **kwargs)
+        self.sentence_attention.to(*args, **kwargs)
+        return self
+
+