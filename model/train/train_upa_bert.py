c9f38edb8342741a054572ccd03d9f1d07f3fe2a
uniquefine <finally@lepus.uberspace.de>
2019-06-04 18:40:18 +0800

    wrote train script for UPABert
    Former-commit-id: 5f212d0a6017f78f7b5a0b62a85cc5173a4169b4 [formerly 90981df8aa07ead74c95eda9b2dc01b0d3668483] [formerly 71e6c2bf09895e3cbeeb57254ac58e5cce5804d2 [formerly 8bd9f218ca58d4efe2cc6bdc76f5d9716f3ed9e1]]
    Former-commit-id: b90a917b7fa6d1bf0219014362e5af78d541152b [formerly 863db90f148c81ddf196174e3ffe7ac5a6058acf]
    Former-commit-id: f9763e097de283b22393298f846203c1f7446698
    Former-commit-id: d9faa01d0a80c607db27e7e79992046556cd83b1

---
+++ model/train/train_upa_bert.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+from model.train.train import parse_args, train
+from utils.data import SentenceMatrixDataset
+from model.upa_bert import UPABert
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    folder = './data/' + args.dataset
+
+    userlist_filename = folder + '/usrlist.txt'
+    productlist_filename = folder + '/prdlist.txt'
+    wordlist_filename = folder + '/wordlist.txt'
+    train_file = folder + '/train.txt'
+    dev_file = folder + '/dev.txt'
+    test_file = folder + '/test.txt'
+
+    train_dat = SentenceMatrixDataset(train_file, userlist_filename, productlist_filename,
+                                      wordlist_filename, force_no_cache=args.force_document_processing)
+    dev_dat = SentenceMatrixDataset(dev_file, userlist_filename, productlist_filename,
+                                    wordlist_filename, force_no_cache=args.force_document_processing)
+
+    n_user = len(train_dat.users)
+    n_product = len(train_dat.products)
+    n_classes = train_dat.get_n_classes()
+
+    model = UPABert(n_user, n_product, args.n_classes, args.user_size,
+                       args.product_size, args.attention_hidden_size)
+    train(model, train_dat, dev_dat, args, use_cat_collate=True)
