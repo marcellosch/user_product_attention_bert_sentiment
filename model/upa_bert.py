6600d935aacbf363f1ce24122d2813af6793ae65
uniquefine <finally@lepus.uberspace.de>
2019-06-04 17:22:18 +0800

    updated upa_bert accept batch, and sentence_matrix
    Former-commit-id: 2d0f5a24c4b9039691432087204f8b77104ad99e [formerly 504753f4a4548ce9e7373f5604fbcca0b05e20ab] [formerly e7624c712b1921095a3df3c1567e0eeb091a9990 [formerly 366674f212589f235058f80e8f0b8a9dcf3aed57]]
    Former-commit-id: e4bde486500849e04d33ca2ef4dd0954ddabda7e [formerly 1082c2facea003fb48fc0a8635d58de6da2fc930]
    Former-commit-id: 7efabe2e902eefa9dd4791ef8864acb07ce6f810
    Former-commit-id: 9642f44437860aa90019ad1be5351cddfe8559df

--- model/upa_bert.py
+++ model/upa_bert.py
@@ -9,30 +9,42 @@ logging.basicConfig(level=logging.INFO, format=log_format)
 
 class UPABert(torch.nn.Module):
 
-    def __init__(self, n_user, n_product, n_classes, user_size=200, product_size=200, hidden_size=768):
+    def __init__(self, n_user, n_product, n_classes, user_size=200, product_size=200, attention_hidden_size=200, hidden_size=768):
+        self.n_user = n_user
+        self.n_product = n_product
+        self.n_classes = n_classes
+        self.user_size = user_size
+        self.product_size = product_size
+        self.attention_hidden_size = attention_hidden_size
+        self.hidden_size = hidden_size
+
         self.Uemb = torch.nn.Embedding(n_user, user_size)
         self.Pemb = torch.nn.Embedding(n_product, product_size)
         self.bert = BertModel.from_pretrained("bert-base-uncased")
-        self.word_attention = UserProductAttention()
+        self.word_attention = UserProductAttention(user_size, product_size, attention_hidden_size, hidden_size)
         self.lstm = torch.nn.LSTM(hidden_size, hidden_size)
         self.sentence_attention = UserProductAttention()
         self.linear = torch.nn.Linear(self.hidden_size, self.n_classes)
-        self.softmax = torch.nn.Softmax()
+        self.softmax = torch.nn.LogSoftmax()
 
-    def forward(self, input_ids, input_mask, user_ids, product_ids, sentence_offsets):
+    def forward(self, batch):
         """ Inputs:
             `input_ids`: Tensor of shape [batch_size, max_seq_length] containing one documents word ids per row
             `input_mask`: torch.LongTensor of shape [batch_size, max_seq_length] with indices in [0,1]. The mask is used to mask sentences that are shorter than max_seq_length
             list of dicts: input_ids, input mask, token_type_ids, user_id, product_id, sentence_offsets
             `user_ids`: torch.LongTensor of shape [batch_size] that denotes the user id for documents
             `product_ids`: torch.LongTensor of shape [batch_size] that denotes the product ids for documents
-            `sentence_offsets`: list of batch_size iterables where each contains the integer offsets of the sentence starts in document 
         """
+        user_ids, product_ids, _, sentence_matrix = batch
+        max_sentence_count = sentence_matrix.shape[0] == 0
         user_embs = self.Uemb(user_ids)
         product_embs = self.Pemb(product_ids)
-        bert_out, _ = self.bert(input_ids, output_all_encoded_layers=False)
+        bert_out, _ = self.bert(sentence_matrix, output_all_encoded_layers=False)
+        repeated_user_embs = user_embs.repeat_interleave(max_sentence_count, dim=0)
+        repeated_product_embs = product_embs.repeat_interleave(max_sentence_count, dim=0)
         word_attention_out = self.word_attention(
-            bert_out, user_embs, product_embs, sentence_offsets)
+            bert_out, repeated_user_embs, repeated_product_embs)
+        word_attention_out = word_attention_out.view(-1, max_sentence_count, self.hidden_size)
         lstm_out, _ = self.lstm(word_attention_out)
         sentence_attention_out = self.sentence_attention(
             lstm_out, user_embs, product_embs)
