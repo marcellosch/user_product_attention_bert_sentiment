f7b94c3cb05a337c3c7860203ae8c017467867cc
uniquefine <finally@lepus.uberspace.de>
2019-06-04 17:09:20 +0800

    revert deletion of file
    Former-commit-id: 536f929db7f8786a0b151613a250993fc0623b9c [formerly bc391fa0fd18861497fe4e3c4a2673396a4779ca] [formerly 5a41198c484e09096b5d113593e1e244a1ad60e8 [formerly ecdee1378a34185dd667f63e37cf59f6ce9e1401]]
    Former-commit-id: bdafadb1ec0047250b3448c70d4ff6fd040393c0 [formerly 047011650dd25e750d873dfa8ac03123364f25f3]
    Former-commit-id: 933c1ff8f9d7055a0d57030ab7254fdb6180d30c
    Former-commit-id: 0db4d46200d5a9a57550feff55bfb5337338b068

---
+++ model/train/train_simple_user_product_bert.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+from model.train.train import parse_args, SentimentDataset, train
+from model.simple_upa_bert import SimpleUPABert
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    folder = './data/' + args.dataset
+
+    userlist_filename = folder + '/usrlist.txt'
+    productlist_filename = folder + '/prdlist.txt'
+    wordlist_filename = folder + '/wordlist.txt'
+    train_file = folder + '/train.txt'
+    dev_file = folder + '/dev.txt'
+    test_file = folder + '/test.txt'
+
+    train_dat = SentimentDataset(train_file, userlist_filename, productlist_filename,
+                                 wordlist_filename, force_no_cache=args.force_document_processing)
+    dev_dat = SentimentDataset(test_file, userlist_filename, productlist_filename,
+                               wordlist_filename, force_no_cache=args.force_document_processing)
+
+    n_user = len(train_dat.users)
+    n_product = len(train_dat.products)
+    n_classes = train_dat.get_n_classes()
+
+    model = SimpleUPABert(n_user, n_product, n_classes, args.user_size,
+                          args.product_size, args.attention_hidden_size)
+    train(model, train_dat, dev_dat, args)
