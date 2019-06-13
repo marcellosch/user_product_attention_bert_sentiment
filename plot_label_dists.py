ebb3d100d44121b083412a1f7c08e9d408cee010
uniquefine <finally@lepus.uberspace.de>
2019-06-13 15:41:05 +0800

    added script to output dataset label distributions in figure
    Former-commit-id: 8a39785dd1d9d007c07c707af68aef33fd7e8745 [formerly a1f0687dbc73f716f2489430480456f833b47b82] [formerly ebe8ca944f74526e29f4dda1463c098baec509e2 [formerly d943c11fa698fdf53e3a6958b34acd104878b44d]]
    Former-commit-id: c0142adb3b4d09e41d9d11c81185088c72487280 [formerly bce211c3f32bf0fbc16183d2bb1368e7a914fc08]
    Former-commit-id: 4e0f807ca7099638a78691ee0af69e2f0ff1460a
    Former-commit-id: b5fc5a69baac5a445d974f73592e5b73c7e6e7d0

---
+++ plot_label_dists.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+### Imports 
+from utils.data import SentimentDataset
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('bmh')
+
+### config variables
+out_file = "figures/label_distributions.png"
+path_template = "data/{}"
+train = 'train.txt'
+wordlist = "wordlist.txt"
+productlist = "prdlist.txt"
+userlist = "usrlist.txt"
+
+datasets = ["IMDB", "yelp13", "yelp14"]
+
+### Load each datset and add a plot for its label distribution
+fig, ax = plt.subplots(1,len(datasets), figsize=(12,3))
+for i, s in enumerate(datasets):
+    path = Path(path_template.format(s))
+    dat = SentimentDataset(str(path/train),
+                       str(path/userlist),
+                       str(path/productlist),
+                       str(path/wordlist))
+    x = np.array(dat.documents["label"])
+    ax[i].hist(x, density=True, align='left', bins=range(max(x)+2), color=f"C{i}") 
+    ax[i].set_xticks(range(max(x)+1))
+    ax[i].set_facecolor((1.0, 1.00, 1.00))
+    ax[i].set_title(s)
+    
+### Save image to folder
+fig.savefig(out_file, dpi=200)
